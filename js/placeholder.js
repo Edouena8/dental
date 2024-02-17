1b5a9a23adcebe93df2f7d53b121ecaab0469eef
Yaroslava Pletsys <103317047+Edouena8@users.noreply.github.com>
2024-02-17 21:34:58 +0100

    created page For patients

--- js/placeholder.js
+++
@@ -1,7 +0,0 @@
-document.querySelector('.form-input').addEventListener('focusin', e => {
-    e.currentTarget.placeholder = e.currentTarget.dataset.placeholder;
-});
-
-document.querySelector('.form-input').addEventListener('focusout', e => {
-    e.currentTarget.placeholder = ' ';
-})
